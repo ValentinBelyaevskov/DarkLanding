990ab2d4f25045e1cfe588dd84f986a01784c2c8
ValentinBelyaevskov <valentinsergeevich99@mail.ru>
2021-10-21 03:06:05 +0700

    create reasons section

--- js/main.js
+++ js/main.js
@@ -7,13 +7,22 @@ const menuHide = document.querySelector(".menu__hide");
 headerBurger.addEventListener("click", () => {
    menu.style.transitionDuration = "0.5s"
    menu.style.left = 0;
+
+   let widthWithScroll = document.body.clientWidth;
    document.body.style.overflow = "hidden";
+
+   let widthWithoutScroll = document.body.clientWidth;
+   let scrollbarWidth = widthWithoutScroll - widthWithScroll
+
+   document.body.style.paddingRight = `${scrollbarWidth}px`
+
 })
 
 menuHide.addEventListener("click", () => {
    menu.style.transitionDuration = "0.5s"
    menu.style.left = "-100vw"
    document.body.style.overflow = "auto";
+   document.body.style.paddingRight = 0;
 })
 
 menu.addEventListener("transitionend", () => menu.style.transitionDuration = "0s");
@@ -26,20 +35,32 @@ const watchPreviewVideo = document.querySelector(".watch-preview__video");
 
 watchPreviewVideo.addEventListener("click", (e) => {
 
+   let videoContainer = e.currentTarget.querySelector(".watch-preview__youtube-video");
+   let videoBackground = e.currentTarget.querySelector(".watch-preview__youtube-background");
+
    if (e.target.closest(".watch-preview__hide")) {
-      videoContainer.style.display = "none";
-      videoBackground.style.display = "none";
-      document.body.style.overflow = "auto";
-      document.body.style.paddingRight = 0;
       videoContainer.style.opacity = 0;
       videoBackground.style.opacity = 0;
+      header.style.zIndex = 3;
+      
+      videoContainer.addEventListener("transitionend", () => {
+         videoContainer.style.display = "none"
+      }, {once: true});
+      videoBackground.addEventListener("transitionend", () => {
+         videoBackground.style.display = "none"
+         document.body.style.paddingRight = 0;
+         document.body.style.overflow = "auto";
+      }, {once: true});
+      console.log("hide");
+      // player.playVideo()
+      player.pauseVideo()
+      player.seekTo(0, false)
       return;
    }
 
    if (e.target.className !== "watch-preview__play-video") return;
 
-   videoContainer = e.currentTarget.querySelector(".watch-preview__youtube-video");
-   videoBackground = e.currentTarget.querySelector(".watch-preview__youtube-background");
+   console.log("show")
 
    header.style.zIndex = 2;
 
