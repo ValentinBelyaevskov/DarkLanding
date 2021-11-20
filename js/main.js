6403204f41b4d57d26bcf315c8fe7b6da35a394c
ValentinBelyaevskov <valentinsergeevich99@mail.ru>
2021-11-20 00:12:41 -0800

    delete object-fit property from iframe

--- js/main.js
+++ js/main.js
@@ -3,15 +3,32 @@
 // * Функции общего назначения
 
 // js functions
-const getValueWithoutMeasurers = value => value.split("").slice(0, -2).join("");
+const getValueWithoutMeasurers = (value, measurerLength) => value.split("").slice(0, - measurerLength).join("");
 
 // DOM functions
 const setWrapperScrollContainerOverflow = overflow => {
    // В safari нельзя задавать для body overflow:hidden. Поэтому создаётся дополнительная
-   // обёртка над wrapper (wrapperContainer). Задать wrapper высоту в 100vh тоже нельзя, т.к. при
-   // клике по видео со скроллом, оно сделает перемотку наверх
+   // обёртка над wrapper (wrapperContainer).
+   // debugger
+   // console.log(document.documentElement.scrollTop);
+
+   const wrapper = document.querySelector(".wrapper");
    const wrapperContainer = document.querySelector(".wrapper-scroll-container");
-   wrapperContainer.style.overflow = overflow;
+   const wrapperContainerTop = `-${document.documentElement.scrollTop}px`;
+   wrapperContainer.style.overflow = overflow === "scroll" ? "unset" : overflow;
+
+   if (overflow == "scroll") {
+      document.documentElement.scrollTop = - getValueWithoutMeasurers(wrapperContainerTop, 2);
+      wrapper.style.top = "0px";
+      return;
+   }
+
+   if (overflow === "unset") {
+      document.documentElement.scrollTop = - getValueWithoutMeasurers(getComputedStyle(wrapper).top, 2);
+   }
+
+   wrapper.style.top = wrapperContainerTop;
+   // На момент скрытия элемента и возврата прокрутки, scrollTop у wrapperContainer равен нулю
 }
 
 const setPagePartsPaddingRight = (haveAScrollbar) => {
@@ -25,6 +42,7 @@ const setPagePartsPaddingRight = (haveAScrollbar) => {
       pageParts.style.paddingRight = 0;
    } else {
       let widthWithScroll = wrapperContainer.clientWidth;
+      // !                                               
       setWrapperScrollContainerOverflow("hidden");
       let widthWithoutScroll = wrapper.clientWidth;
       let scrollbarWidth = widthWithoutScroll - widthWithScroll;
@@ -56,7 +74,7 @@ const menu = document.querySelector(".menu");
 const header = document.querySelector(".header");
 const burgerButton = document.querySelector(".header__burger");
 const menuHideButton = document.querySelector(".menu__hide");
-const clients = document.querySelector(".clients");
+const reasons = document.querySelector(".reasons");
 
 const setMenuTransitionDuration = transitionDuration => menu.style.transitionDuration = transitionDuration;
 
@@ -77,7 +95,8 @@ const showMenu = () => {
 
 const hideMenu = (time) => {
    setMenuStyle(time, "-100vw", () => {
-      setWrapperScrollContainerOverflow("visible");
+      // !                                                           
+      setWrapperScrollContainerOverflow("unset");
    });
    setPagePartsPaddingRight(true);
 }
@@ -85,7 +104,6 @@ const hideMenu = (time) => {
 // events--------------------------------------------------------------
 burgerButton.addEventListener("click", () => {
    showMenu();
-   console.log("clickBurger");
 });
 menuHideButton.addEventListener("click", () => hideMenu("0.5s"));
 menu.addEventListener("transitionend", () => setMenuTransitionDuration("0s"));
@@ -103,37 +121,36 @@ const videoBackground = document.querySelector(".watch-preview__youtube-backgrou
 
 const setHeaderZIndex = index => header.style.zIndex = index;
 
-const setClientsZIndex = index => {
-   clients.style.zIndex = index
-};
+const setReasonsZIndex = index => reasons.style.zIndex = index;
 
 const videoJustifiedAlignment = () => {
    youtubeVideo.style.padding = "0 0 calc(56.25% * 0.8) 0";
    youtubeVideo.style.width = "80%";
+   youtubeVideo.style.height = "0";
 }
 
 const videoHeightAlignment = () => {
    youtubeVideo.style.padding = "0 0 60vh 0";
    youtubeVideo.style.width = "calc(60vh * (16 / 9))";
+   youtubeVideo.style.height = "0";
 }
 
 const videoContainerAlingment = () => {
    youtubeVideo.style.width = "100%";
-   youtubeVideo.style.padding = "0 0 56.25% 0";
+   youtubeVideo.style.height = "100%";
+   youtubeVideo.style.padding = "0";
 }
 
 const videoBighWidthAlingment = () => {
    youtubeVideo.style.width = "800px";
    youtubeVideo.style.padding = "0 0 450px 0";
+   youtubeVideo.style.height = "0";
 }
 
-// ! VIDEO SIZE                                                      
 const setVideoSize = (orientationChange) => {
-   const width = orientationChange ? document.documentElement.clientHeight : document.documentElement.clientWidth;
-   const height = orientationChange ? document.documentElement.clientWidth : document.documentElement.clientHeight;
+   const width = orientationChange ? document.documentElement.clientHeight : window.innerWidth;
+   const height = orientationChange ? window.innerWidth : document.documentElement.clientHeight;
    const ratio = width / height;
-
-   // console.log(orientationChange, width, height);
 
    if (width > 1024) {
       videoBighWidthAlingment();
@@ -144,6 +161,11 @@ const setVideoSize = (orientationChange) => {
    } else if (ratio > (1024 / 768)) {
       videoHeightAlignment();
    }
+}
+
+const setWrapperScrollContainerHeight = height => {
+   const wrapperContainer = document.querySelector(".wrapper-scroll-container");
+   wrapperContainer.style.height = height;
 }
 
 const showElement = (element, showClass, animate, callback) => {
@@ -174,38 +196,38 @@ const hideElement = (element, showClass, animate, callback) => {
    }
 }
 
-const hideLandscapeVideo = (video, videoBackground, animate) => {
+const hideLandscapeVideo = (video, videoBackground, animate, setWrapperContainerOverflow) => {
    hideElement(video, "show-video", animate);
    hideElement(videoBackground, "show-video-background", animate, () => {
       setPagePartsPaddingRight(true);
-      setWrapperScrollContainerOverflow("visible");
+      // !                                                     
+      if (setWrapperContainerOverflow) setWrapperScrollContainerOverflow("unset");
       setHeaderZIndex(3);
-      // для android.
-      screen.orientation.unlock();
+      setWrapperScrollContainerHeight("100%");
       returnToStartingVideo();
-      setClientsZIndex(2);
+      setReasonsZIndex(2);
    });
    pauseVideo();
 }
 
 const showLandscapeVideo = (video, videoBackground, animate) => {
    showElement(video, "show-video", animate, () => {
-      // ! VIDEO SIZE                                                      
       setVideoSize();
    });
    showElement(videoBackground, "show-video-background", animate, () => {
       setHeaderZIndex(2);
       setPagePartsPaddingRight(false);
-      setClientsZIndex(1);
+      setReasonsZIndex(3);
+      setWrapperScrollContainerHeight("100vh");
    });
 }
 
 // event listeners
 const watchPreviewVideoClickHandler = e => {
-   if (document.documentElement.clientWidth <= 414) return;
+   if (window.innerWidth <= 414) return;
 
    if (e.target.closest(".watch-preview__hide")) {
-      hideLandscapeVideo(youtubeVideo, videoBackground, true);
+      hideLandscapeVideo(youtubeVideo, videoBackground, true, true);
       return;
    }
 
@@ -215,102 +237,45 @@ const watchPreviewVideoClickHandler = e => {
 }
 
 const windowResizeHandler = () => {
-   // debugger
-   console.log(getComputedStyle(youtubeVideo).display)
    // При ширине экрана больше 414px и если видео не проигрывается
    // скрыть видео
-   if (document.documentElement.clientWidth > 414
+   if (window.innerWidth > 414
       && (getVideoStatus() === 5 || getVideoStatus() === -1)
       && youtubeVideo.classList.contains("show-video")) {
-      // hideElement(youtubeVideo, "show-video", false);
-      hideLandscapeVideo(youtubeVideo, videoBackground, false)
-      console.log("hide pause video in large display")
+      hideLandscapeVideo(youtubeVideo, videoBackground, false, false)
+      // console.log("hide pause video in large display")
    }
 
    // Если видео не проигрывается и ширина экрана больше
    // чем 414px, прекратить обработку
-   if ( (getVideoStatus() === 5 || getVideoStatus() === -1)
-      && document.documentElement.clientWidth > 414 ) return
-   // ! VIDEO SIZE                                                   
+   if ((getVideoStatus() === 5)
+      && window.innerWidth > 414) return
    setVideoSize();
 
    // Если ширина экрана меньше 414px, скрыть фон видео
-   if (document.documentElement.clientWidth <= 414) {
+   if (window.innerWidth <= 414) {
       hideElement(videoBackground, "show-video-background", false, () => {
          setPagePartsPaddingRight(true);
-         setWrapperScrollContainerOverflow("visible");
+         //                                        !
+         // Здесь не нужно изменять свойство overflow у wrapperContainer
+         setWrapperScrollContainerOverflow("scroll");
+         // console.log("call setWrapperScrollContainerOverflow");
          setHeaderZIndex(3);
-
       });
       showElement(youtubeVideo, "show-video", false);
    }
 
    // Если ширина экрана меньше 414px и видео проигрывается,
    // показать видео вместе с фоном
-   if (document.documentElement.clientWidth > 414
-      && (getVideoStatus() === 1 || getVideoStatus() === 2)) {
+   if (window.innerWidth > 414
+      && (getVideoStatus() === 1 || getVideoStatus() === 2
+      || getVideoStatus() === -1 || getVideoStatus() === 0
+      || getVideoStatus() == 3)) {
       showLandscapeVideo(youtubeVideo, videoBackground, false);
-      console.log("w > 414 & video play/pause")
+      // console.log("w > 414 & video play/pause")
    }
-
-
-
-
-   //    showElement(youtubeVideo, "show-video", false);
-
-   // } else if (screen.orientation.type === "portrait-primary" && document.documentElement.clientWidth > 414) {
-   //    if (getVideoStatus() === 5) {
-   //       hideElement(youtubeVideo, "show-video", false);
-   //       hideElement(videoBackground, "show-video-background", false);
-   //    }
-
-   //    if (getVideoStatus() === 1 || getVideoStatus() === 2) {
-   //       showElement(videoBackground, "show-video-background", false);
-   //       setHeaderZIndex(2);
-   //    }
-   // }
 }
 
-// const orientationChangeHandler = () => {
-
-//    // ! VIDEO SIZE                                                         
-//    // setVideoSize(true);
-
-//    // ! window.matchMedia("(orientation: portrait)");
-//    if (screen.orientation.type === "landscape-primary") {
-//       if (getVideoStatus() === 1 || getVideoStatus() === 2) {
-
-//          // !
-//          screen.orientation.unlock();
-
-//          showElement(youtubeVideo, "show-video", false);
-//          showElement(videoBackground, "show-video-background", false, () => {
-//             setHeaderZIndex(2);
-//             setPagePartsPaddingRight(false);
-
-//          });
-//       }
-
-//       if (getVideoStatus() === 5) {
-//          hideElement(youtubeVideo, "show-video", false);
-//          hideElement(videoBackground, "show-video-background", false);
-
-//       }
-//    }
-
-//    // ! window.matchMedia("(orientation: portrait)");
-//    if (screen.orientation.type === "portrait-primary" && document.documentElement.clientWidth <= 414) {
-//       hideElement(videoBackground, "show-video-background", false, () => {
-//          setPagePartsPaddingRight(true);
-//          setWrapperScrollContainerOverflow("visible");
-//          setHeaderZIndex(3);
-//       });
-
-//       if (getVideoStatus() === 5) {
-//          showElement(youtubeVideo, "show-video", false);
-//       }
-//    }
-// }
 
 // events------------------------------------------------------------
 watchPreviewVideo.addEventListener("click", watchPreviewVideoClickHandler);
@@ -320,10 +285,6 @@ watchPreviewVideo.addEventListener("touchstart", e => {
    watchPreviewVideoClickHandler(e);
 });
 window.addEventListener("resize", windowResizeHandler);
-
-// ! document.addEventListener("orientationchange", updateOrientation);
-// screen.orientation.addEventListener('change', orientationChangeHandler);
-
 
 
 // ? include clients elements ----------------------------------------------
